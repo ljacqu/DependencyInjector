6c9632974ceb4e5029be75e9f229b4f2c8fbba2a
ljacqu <ljacqu@users.noreply.github.com>
2016-06-21 21:27:07 +0200

    Reduce Lombok to test scope

--- src/main/java/ch/jalu/injector/testing/runner/PendingInstantiation.java
+++ src/main/java/ch/jalu/injector/testing/runner/PendingInstantiation.java
@@ -1,19 +1,28 @@
 package ch.jalu.injector.testing.runner;
 
 import ch.jalu.injector.instantiation.Instantiation;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.lang.reflect.Field;
 
 /**
  * Contains an instantiation and the field it's for.
  */
-@Getter
-@AllArgsConstructor
 class PendingInstantiation {
 
     private final Field field;
     private final Instantiation<?> instantiation;
+
+    public PendingInstantiation(Field field, Instantiation<?> instantiation) {
+        this.field = field;
+        this.instantiation = instantiation;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Instantiation<?> getInstantiation() {
+        return instantiation;
+    }
 
 }
