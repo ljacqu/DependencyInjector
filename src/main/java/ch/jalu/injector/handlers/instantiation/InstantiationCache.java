662002267399cd1899a3a647f1a88ac7e496c11d
ljacqu <ljacqu@users.noreply.github.com>
2017-02-05 14:27:28 +0100

    Close #1 Implement instantiation cache - Not included by default because it's a primitive Map structure

---
+++ src/main/java/ch/jalu/injector/handlers/instantiation/InstantiationCache.java
@@ -0,0 +1,53 @@
+package ch.jalu.injector.handlers.instantiation;
+
+import ch.jalu.injector.context.InstantiationContext;
+import ch.jalu.injector.context.ResolvedInstantiationContext;
+import ch.jalu.injector.context.StandardResolutionType;
+import ch.jalu.injector.context.UnresolvedInstantiationContext;
+import ch.jalu.injector.handlers.postconstruct.PostConstructHandler;
+
+import javax.annotation.Nullable;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Primitive "cache" for instantiation methods. It is recommended to use a more suitable
+ * cache structure instead, e.g. with Guava's CacheBuilder. As such, this handler is not
+ * included by default.
+ */
+public class InstantiationCache implements InstantiationProvider, PostConstructHandler {
+
+    protected Map<String, WeakReference<Instantiation<?>>> entries = new ConcurrentHashMap<>();
+
+    @Override
+    public <T> Instantiation<? extends T> get(UnresolvedInstantiationContext<T> context) {
+        return getInstantiation(context);
+    }
+
+    @Override
+    public <T> T process(T object, ResolvedInstantiationContext<T> context) throws Exception {
+        if (shouldCacheMethod(context) && getInstantiation(context) == null) {
+            entries.put(context.getMappedClass().getCanonicalName(),
+                new WeakReference<Instantiation<?>>(context.getInstantiation()));
+        }
+        return null;
+    }
+
+    @Nullable
+    @SuppressWarnings("unchecked")
+    private <T> Instantiation<? extends T> getInstantiation(InstantiationContext<T> context) {
+        WeakReference<Instantiation<?>> instantiation = entries.get(context.getMappedClass().getCanonicalName());
+        return instantiation == null ? null : (Instantiation) instantiation.get();
+    }
+
+    /**
+     * Returns whether the instantiation method from the given context should be cached.
+     *
+     * @param context the context to process
+     * @return true to cache the instantiation method, false otherwise
+     */
+    protected boolean shouldCacheMethod(ResolvedInstantiationContext<?> context) {
+        return context.getResolutionType() == StandardResolutionType.REQUEST_SCOPED;
+    }
+}
---
+++ src/test/java/ch/jalu/injector/handlers/instantiation/InstantiationCacheTest.java
@@ -0,0 +1,98 @@
+package ch.jalu.injector.handlers.instantiation;
+
+import ch.jalu.injector.Injector;
+import ch.jalu.injector.InjectorBuilder;
+import ch.jalu.injector.handlers.Handler;
+import ch.jalu.injector.samples.AlphaService;
+import ch.jalu.injector.samples.BetaManager;
+import ch.jalu.injector.samples.GammaService;
+import ch.jalu.injector.samples.ProvidedClass;
+import ch.jalu.injector.utils.ReflectionUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+/**
+ * Test for {@link InstantiationCache}.
+ */
+public class InstantiationCacheTest {
+
+    private Injector injector;
+    private DefaultInjectionProvider defaultInjectionProvider;
+    private InstantiationCache instantiationCache;
+
+    @Before
+    public void setUpInjector() {
+        List<Handler> handlers = InjectorBuilder.createDefaultHandlers("ch.jalu.injector.samples");
+        InstantiationCache instantiationCache = new InstantiationCache();
+        handlers.add(0, instantiationCache);
+
+        List<Handler> handlerSpies = new ArrayList<>(handlers.size());
+        for (Handler handler : handlers) {
+            Handler spy = spy(handler);
+            if (spy instanceof DefaultInjectionProvider) {
+                this.defaultInjectionProvider = (DefaultInjectionProvider) spy;
+            } else if (spy instanceof InstantiationCache) {
+                this.instantiationCache = (InstantiationCache) spy;
+            }
+            handlerSpies.add(spy);
+        }
+        this.injector = new InjectorBuilder().addHandlers(handlerSpies).create();
+    }
+
+    @Test
+    public void shouldNotCacheInstantiation() {
+        // given
+        injector.register(ProvidedClass.class, new ProvidedClass(""));
+        injector.getSingleton(GammaService.class);
+        injector.getSingleton(AlphaService.class);
+
+        // when / then
+        assertThat(getCacheMap(), anEmptyMap());
+    }
+
+    @Test
+    public void shouldCacheRequestScopedInstantiations() {
+        // given
+        injector.register(ProvidedClass.class, new ProvidedClass(""));
+        injector.getSingleton(GammaService.class);
+        Mockito.reset(defaultInjectionProvider);
+
+        // when
+        BetaManager manager1 = injector.newInstance(BetaManager.class);
+        BetaManager manager2 = injector.newInstance(BetaManager.class);
+
+        // then
+        assertThat(manager1, not(sameInstance(manager2)));
+        assertThat(getCacheMap(), aMapWithSize(1));
+        assertThat(getCacheMap().get(BetaManager.class.getCanonicalName()), not(nullValue()));
+        verify(defaultInjectionProvider, times(1)).safeGet(any(Class.class));
+    }
+
+    private Map<String, WeakReference<Instantiation>> getCacheMap() {
+        try {
+            Field field = InstantiationCache.class.getDeclaredField("entries");
+            return (Map) ReflectionUtils.getFieldValue(field, instantiationCache);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
